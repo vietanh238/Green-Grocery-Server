b4451615aba2515430f6505941146c0fe6dd434a
vietanh238 <vietanh.duong.238@gmail.com>
2025-09-11 06:16:49 +0700

    refactor: refactor message response

--- Server/settings.py
+++ Server/settings.py
@@ -18,14 +18,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'corsheaders',
+    'rest_framework',
     'accounts'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -33,10 +34,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
 
 ROOT_URLCONF = 'Server.urls'
 
@@ -122,7 +125,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(100),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
--- accounts/login/views.py
+++ accounts/login/views.py
@@ -11,30 +11,58 @@ class LoginView(APIView):
     #     user = request.
 
     def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data['user']
-            if not user.is_active:
-                return Response({
-                    'status': '2',
-                    'message': 'Account is not active'
+        try:
+            serializer = LoginSerializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.validated_data['user']
+                if not user.is_active:
+                    return Response({
+                        'status': '2',
+                        'response': {
+                            "error_code": "1",
+                            "error_message": "User is not active"
+                        }
+                    })
+
+                refresh = RefreshToken.for_user(user)
+
+                response = Response({
+                    'status': '1',
+                    'response': {
+                        'access': str(refresh.access_token),
+                        'user': {
+                            'id': user.id,
+                            'phone_number': user.phone_number
+                        }
+                    }
                 })
 
-            refresh = RefreshToken.for_user(user)
+                response.set_cookie(
+                    key='refresh_token',
+                    value=str(refresh),
+                    httponly=True,
+                    secure=True,
+                    samesite='None',
+                    max_age=7*24*60*60,
+                    path='/api/account/refresh/'
+                )
 
-            return Response({
-                'status': '1',
-                'data': {
-                    'access': str(refresh.access_token),
-                    'refresh': str(refresh),
-                    'user': {
-                        'id': user.id,
-                        'phone_number': user.phone_number
+                return response
+
+            else:
+                return Response({
+                    'status': '2',
+                    'response': {
+                        "error_code": "1",
+                        "error_message": "Account incorrect"
                     }
+                })
+
+        except Exception as ex:
+            return Response({
+                'status': '2',
+                'response': {
+                    "error_code": "9999",
+                    "error_message": "System error"
                 }
             })
-
-        return Response({
-            'status': '2',
-            'message': 'Incorrect account'
-        })
